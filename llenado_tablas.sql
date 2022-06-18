95975c7b31a2ca00f299cb2b34e752580ef1c0b8
metalkutz <metalkutz@gmail.com>
2022-06-18 14:50:33 +0200

    creacion archivos para el llenado de tablas y consultas

---
+++ llenado_tablas.sql
@@ -0,0 +1,4 @@
+/*
+Una vez que has creado el schema, inventa valores y rellena las tablas con datos apropiados.
+Trata de tener columnas suficientes para almacenar toda la información que podría ser relevante.
+*/
