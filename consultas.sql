c2023bc8a6a54951ed5626e3e6468c651bca9f13
metalkutz <metalkutz@gmail.com>
2022-07-09 20:40:51 +0200

    cambio menor nombre archivo

---
+++ consultas.sql
@@ -0,0 +1,15 @@
+/*
+Al terminar tienes que ser capaz de realizar las siguientes consultas.
+
+		Nota media de cada asignatura.
+			Nota media asociada a cada profesor (media las notas de sus asignaturas)
+		¿Cuál es la nota del último examen realizado por el alumno con mayor nota media?
+		¿Cuál es la asignatura más dificil?
+		Busca el grupo de riesgo de cada asignatura (alumnos que peor van) y verifica si hay alguno que esté en todas.
+*/
+
+###### INDICO QUE VOY A TRABAJAR SOBRE LA NUEVA BBDD 
+use BBDD_IDBootcamps;
+
+
+select * from ciudades;
